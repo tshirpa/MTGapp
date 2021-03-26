75e3c34a4ef25d406a8bce85842e16d9b6ccd096
tshirpa <79044535+tshirpa@users.noreply.github.com>
2021-03-26 14:37:39 +0100

    Update update_db.py

--- update_db.py
+++ update_db.py
@@ -1,7 +1,7 @@
 #This needs to be ran before the application can run
 from mtgtools.MtgDB import MtgDB
 from pathlib import Path
-db_path = Path("~/Documents/'my_db.fs'").expanduser()
+db_path = Path("~/Documents/my_db.fs").expanduser()
 db_path_str = str(db_path)
 mtg_db = MtgDB(db_path_str)
 mtg_db.mtgio_update()
